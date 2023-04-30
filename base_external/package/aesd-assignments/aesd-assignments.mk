abf1604dc0538b977673f9e4da2c317f9c0505f0
aditi <adna4700@colorado.edu>
2023-04-29 21:05:03 -0600

    lastest commit added

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 90dcec4c315b7c0c2396f172c5c874393b82a60c
+AESD_ASSIGNMENTS_VERSION = 9c35825d2ff9d724476f94dbbe7f93e64bf5b6e1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,13 +15,14 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/mq7 all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	
-	$(INSTALL) -m 0755 $(@D)/mq7/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server/* $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
